3edbd6fb5bdea459d413827db011da4194270cf4
Peter Griffin <codezz-ops@protonmail.com>
2025-10-10 02:15:49 -0400

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
   trailingSlash: true,
-  assetPrefix: "/codezz-ops.vercel.app",
-  basePath: "/codezz-ops.vercel.app",
   images: {
     unoptimized: true,
   },
