c1331a1c812e1a4d53b0849c511924f4fa5e4131
Peter Griffin <codezz-ops@protonmail.com>
2025-10-10 02:12:57 -0400

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
   trailingSlash: true,
-  assetPrefix: "/.",
-  basePath: "/."
+  assetPrefix: ".",
+  basePath: ".",
   images: {
     unoptimized: true,
   },
